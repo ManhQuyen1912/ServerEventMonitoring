4e6f64c5798a53480ffa17616b4098d54ce32b6c
ManhQuyen1912 <nmquyen1912@gmail.com>
2024-03-13 04:45:59 +0700

    has basic entry, button

--- app.py
+++ app.py
@@ -1,41 +1,69 @@
-from tkinter import *
+from tkinter import * 
+from ttkbootstrap.constants import *
 import ttkbootstrap as tb
 
 root = tb.Window(themename="darkly")
 root.title("Server Monitor Event Software")
-# root.geometry("800x600")
+root.maxsize(800, 600)
+root.configure(background="white")
 
-# frame = tb.Frame(root, width= 800, height=600,bootstyle="light")
-# frame.pack(pady=20)
-
-# my_label = tb.Label(frame, text="Event Type",font=("Helvetica",12))
-# my_label.pack(pady=10,padx=20,anchor=W)
-
-# evt_Cate = tb.Label(frame,text="Event Category",font=("helvatica",12))
-# evt_Cate.pack(pady=10,padx=20,anchor=E)
-root.maxsize(800, 600)  # specify the max size the window can expand to
-
-# Create left and right frames
-left_frame = Frame(root, width=400, height=600, bg='grey')
-left_frame.grid(row=0, column=0, padx=10, pady=5)
+# Create Frame
+left_frame = Frame(root, width=400, height=600)
+left_frame.grid(row=0, column=0, padx=10, pady=10)
+left_frame.configure(background='gray49')
 
 # Create frames and labels in left_frame
-Label(left_frame, text="Event Category").grid(row=0, column=0, padx=5, pady=5)
-entry1=tb.Entry(left_frame,font=("helvatica",12),width=15).grid(row=1,column=0,sticky=E,pady=5,padx=20)
+evtCat= tb.Label(left_frame, 
+                text="Event Category",
+                font=("helvatica",13),
+                bootstyle='light, inverse').grid(row=0, column=0, padx=5, pady=5)
+entry1= tb.Entry(left_frame,
+                font=("helvatica",12), 
+                width=15).grid(row=1,column=0,sticky=E,pady=5,padx=20)
 
-Label(left_frame, text="Time Generated").grid(row=0, column=1, padx=5, pady=5)
-entry2=tb.Entry(left_frame,font=("helvatica",12),width=15).grid(row=1,column=1,sticky=W,pady=5,padx=20)
+srcName= tb.Label(left_frame, 
+                text="Source Name",
+                font=("helvatica",13),
+                bootstyle='light, inverse').grid(row=0, column=1, padx=5, pady=5)
+entry2= tb.Entry(left_frame,
+                font=("helvatica",12),
+                width=15).grid(row=1,column=1,sticky=W,pady=5,padx=20)
 
-Label(left_frame, text="Source Name").grid(row=2, column=0, padx=5, pady=5)
-entry3=tb.Entry(left_frame,font=("Helvatica",12),width=15).grid(row=3,column=0,sticky=E,pady=5,padx=20)
+timeSt= tb.Label(left_frame, 
+                text="Time Start",
+                font=("helvatica",13),
+                bootstyle='light, inverse').grid(row=2, column=0, padx=5, pady=5)
+entry3= tb.Entry(left_frame,
+                font=("Helvatica",12),
+                width=15).grid(row=3,column=0,sticky=E,pady=5,padx=20)
 
-Label(left_frame, text="Event ID").grid(row=2, column=1, padx=5, pady=5)
-entry4=tb.Entry(left_frame,font=("Helvatica",12),width=15).grid(row=3,column=1,sticky=W,pady=5,padx=20)
+tiemEnd= tb.Label(left_frame, 
+                  text="Time End",
+                  font=("helvatica",13),
+                  bootstyle='light, inverse').grid(row=2, column=1, padx=5, pady=5)
+entry3=tb.Entry(left_frame,
+                font=("Helvatica",12),
+                width=15).grid(row=3,column=1,sticky=W,pady=5,padx=20)
 
-Label(left_frame, text="Event Type").grid(row=4,column=0,pady=5, padx=5)
+evtID= tb.Label(left_frame, 
+                text="Event ID",
+                font=("helvatica",13),
+                bootstyle='light, inverse').grid(row=4, column=0, padx=5, pady=5)
+entry4=tb.Entry(left_frame,
+                font=("Helvatica",12),
+                width=15).grid(row=5,column=0,sticky=E,pady=5,padx=20)
+
+evtType= tb.Label(left_frame, 
+                  text="Event Type",
+                  font=("helvatica",13),
+                  bootstyle='light, inverse').grid(row=4,column=1,pady=5, padx=5)
 eventType=["Information","Warning","Error","Audit Success","Audit Failure","Unknown"]
-comboBox= tb.Combobox(left_frame,bootstyle="success",values = eventType,width=18).grid(row=5,column=0,sticky=W,pady=5,padx=20)
+comboBox= tb.Combobox(left_frame,bootstyle="success",
+                      values = eventType,
+                      width=18).grid(row=5,column=1,sticky=E,pady=5,padx=20)
 
+# Colors: default, primary, secondary, success, info, warning, danger, light, dark
+button = tb.Button(text="Search",bootstyle="secondary,outline").grid(pady=20,padx=5)
 # set combo default
 # comboBox.current(0)
 
