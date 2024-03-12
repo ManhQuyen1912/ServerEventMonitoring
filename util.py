f14ddc42adf6893b1ffadde209c1767294a43c1c
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2024-03-12 11:46:30 +0700

    styling log file

--- util.py
+++ util.py
@@ -15,6 +15,59 @@ def evtTypeToString(record):
     else:
         return 'Unknown'
     
+def evtCtgrToString(record):
+    if record.EventCategory == 0:
+        return 'None'
+    elif record.EventCategory == 1:
+        return 'Device Driver'
+    elif record.EventCategory == 2:
+        return 'Service'
+    elif record.EventCategory == 3:
+        return 'File System'
+    elif record.EventCategory == 4:
+        return 'Object Access'
+    elif record.EventCategory == 5:
+        return 'Registry'
+    elif record.EventCategory == 6:
+        return 'Kernel'
+    elif record.EventCategory == 7:
+        return 'Process'
+    elif record.EventCategory == 8:
+        return 'Security'
+    elif record.EventCategory == 9:
+        return 'System'
+    elif record.EventCategory == 10:
+        return 'Printer'
+    elif record.EventCategory == 11:
+        return 'Initialization'
+    elif record.EventCategory == 12:
+        return 'Service Control Manager'
+    elif record.EventCategory == 13:
+        return 'Directory Service Access'
+    elif record.EventCategory == 14:
+        return 'Directory Service Changes'
+    elif record.EventCategory == 15:
+        return 'Kerberos'
+    elif record.EventCategory == 16:
+        return 'Active Directory'
+    elif record.EventCategory == 17:
+        return 'Account Logon'
+    elif record.EventCategory == 18:
+        return 'Account Management'
+    elif record.EventCategory == 19:
+        return 'Policy Change'
+    elif record.EventCategory == 20:
+        return 'Object Access'
+    elif record.EventCategory == 21:
+        return 'Privilege Use'
+    elif record.EventCategory == 22:
+        return 'Detailed Tracking'
+    elif record.EventCategory == 23:
+        return 'Logon/Logoff'
+    elif record.EventCategory == 24:
+        return 'System'
+    elif record.EventCategory >= 25:
+        return 'Miscellaneous'
 def printRecord(record):
     print('Event Category:', record.EventCategory)
     print('Time Generated:', record.TimeGenerated)
@@ -23,6 +76,17 @@ def printRecord(record):
     print('Event Type:', evtTypeToString(record))
     print('Event Strings:', record.StringInserts)
     print()
+
+def recordToStr(record):
+    category = evtCtgrToString(record)
+    time = str(record.TimeGenerated)
+    source = record.SourceName
+    eventID = str(record.EventID)
+    eventType = evtTypeToString(record)
+    eventStrings = str(record.StringInserts)
+
+    res = category + '\t\t\t' + time + '\t\t' + source + '\t\t' + eventID + '\t' + eventType + '\t\t' + eventStrings + '\n'
+    return res
 
 def logRecord(record):
     #settings
@@ -34,12 +98,13 @@ def logRecord(record):
     filename = 'log_' + currdate.strftime('%m-%Y') + '.txt'
 
     with open(directory + filename, 'a') as file:
-        file.write('Event Category: ' + str(record.EventCategory) + '\n')
-        file.write('Time Generated: ' + str(record.TimeGenerated) + '\n')
-        file.write('Source Name: ' + str(record.SourceName) + '\n')
-        file.write('Event ID: ' + str(record.EventID) + '\n')
-        file.write('Event Type: ' + evtTypeToString(record) + '\n')
-        file.write('Event Strings: ' + str(record.StringInserts) + '\n\n')
+        #if file is empty, write header
+        if file.tell() == 0:
+            #header
+            file.write('Event Category\tTime Generated\t\t\tSource Name\t\t\t\t\tEvent ID\tEvent Type\t\tEvent Strings\n')
+
+        #write record
+        file.write(recordToStr(record))
+        #close
         file.close()
 
-        
