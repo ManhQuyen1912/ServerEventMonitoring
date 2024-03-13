805c37094d659e493fa4d03cc2ceed1d02d9c2c2
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2024-03-13 09:55:54 +0700

    stablize dev

--- filter.py
+++ filter.py
@@ -4,22 +4,26 @@ import datetime
 
 annouceFilterList = [[]]
 
+
 def addFilter(evtCategory, dateStart, dateEnd, timeStart, timeEnd, sourceName, evtID, evtType, action):
     annouceFilterList.append([evtCategory, dateStart, dateEnd, timeStart, timeEnd, sourceName, evtID, evtType, action])
 
-def timeLimitCheck(timeRecord, timeStart, timeEnd):
+def timeLimitCheck(timeRecord, dateStart, dateEnd, timeStart, timeEnd):
     timeRC = datetime.strptime(timeRecord, "%Y-%m-%d %H:%M:%S")
     dateRc = timeRC.date()
     timeRc = timeRC.time()
+
+
 
     
 
 
 def filter(record):
-    for filter in annouceFilterList:
-        #TODO: finish filter condition
-        if (filter[0] == record.EventCategory or filter[0]=='') and 1==1:
-            return action(filter[5])
+    print('announceFilterList' + annouceFilterList)
+    # for filter in annouceFilterList:
+    #     #TODO: finish filter condition
+    #     if (filter[0] == record.EventCategory or filter[0]=='') and 1==1:
+    #         return action(filter[5])
         
 
 def action(action):
