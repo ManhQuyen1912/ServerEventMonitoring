9e90fad28f7f513bdc3f0b021cc102e76a9d3320
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2024-03-14 15:01:41 +0700

    complete SQL query

--- SqlService.py
+++ SqlService.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
+import win32evtlog
+import util
 
 config = {
     'user': 'root',
@@ -23,3 +25,23 @@ cursor = cnx.cursor()
 
 def query():
   return
+
+def insert(record):
+  add_record = ("INSERT INTO events "
+               "(idEvents, Category, `Time generated`, `Source name`, ID, Type, Strings) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+  # use the current number of record in database as id
+  getNumRecord = ("SELECT COUNT(*) FROM events")
+  cursor.execute(getNumRecord)
+  id = str(cursor.fetchone()[0] + 1)
+
+  category = util.evtCtgrToString(record)
+  time = str(record.TimeGenerated)
+  source = record.SourceName
+  eventID = str(record.EventID)
+  eventType = util.evtTypeToString(record)
+  eventStrings = str(record.StringInserts)
+  
+  data_record = (id, category, time, source, eventID, eventType, eventStrings)
+  cursor.execute(add_record, data_record)
+  cnx.commit()
