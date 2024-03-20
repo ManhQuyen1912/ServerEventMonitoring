05ee55b78d41d4cdb2753ce91a5d06c0b02aa7ce
minhhhuydao2003 <daominhhuy.9a1@gmail.com>
2024-03-20 09:25:56 +0700

    add search interface controller

--- controller.py
+++ controller.py
@@ -1,8 +1,12 @@
 from filter import addFilter
+from table import insert
 
 def addFilterInterface(evtCategory, dateStart, dateEnd, timeStart, timeEnd, sourceName, evtID, evtType, action):
     addFilter(evtCategory, dateStart, dateEnd, timeStart, timeEnd, sourceName, evtID, evtType, action)
     return
 
-def searchRcInterface():
+def searchRcInterface(evtCategory, dateStart, dateEnd, timeStart, timeEnd, sourceName, evtID, evtType, action):
+    res = search(evtCategory, dateStart, dateEnd, timeStart, timeEnd, sourceName, evtID, evtType, action)
+    for record in res:
+        insert(record)
     return
